268a293c69766783af035ec23f31010c50a056ae
protolambda <proto@protolambda.com>
2020-01-02 20:49:11 +0100

    update for new pymerkles version

--- test_libs/pyspec/eth2spec/utils/ssz/ssz_impl.py
+++ test_libs/pyspec/eth2spec/utils/ssz/ssz_impl.py
@@ -1,10 +1,10 @@
 from pymerkles.core import View
-from pymerkles.tree import Root, merkle_hash
+from pymerkles.tree import merkle_hash
 
 
 def serialize(obj: View) -> bytes:
     return obj.encode_bytes()
 
 
-def hash_tree_root(obj: View) -> Root:
+def hash_tree_root(obj: View) -> bytes:
     return obj.get_backing().merkle_root(merkle_hash)
--- test_libs/pyspec/eth2spec/utils/ssz/ssz_typing.py
+++ test_libs/pyspec/eth2spec/utils/ssz/ssz_typing.py
@@ -1,21 +1,8 @@
+# flake8: noqa
+# Ignore linter: This module makes importing SSZ types easy, and hides away the underlying library from the spec.
+
 from pymerkles.complex import Container, Vector, List
-from pymerkles.basic import boolean, uint, uint8, uint16, uint32, uint64, uint128, uint256
-from pymerkles.bitfields import BitVector as Bitvector, BitList as Bitlist
-from pymerkles.byte_vector import ByteVector
+from pymerkles.basic import boolean, bit, uint, byte, uint8, uint16, uint32, uint64, uint128, uint256
+from pymerkles.bitfields import Bitvector, Bitlist
+from pymerkles.byte_vector import ByteVector, Bytes1, Bytes4, Bytes8, Bytes32, Bytes48, Bytes96
 from pymerkles.core import BasicView, View, TypeDef
-
-
-class bit(boolean):
-    pass
-
-
-byte = uint8
-
-# Define special Byte vector view types, these are bytes-like:
-# raw representation instead of backed by a binary tree. Inheriting Python "bytes"
-Bytes1 = ByteVector[1]
-Bytes4 = ByteVector[4]
-Bytes8 = ByteVector[8]
-Bytes32 = ByteVector[32]
-Bytes48 = ByteVector[48]
-Bytes96 = ByteVector[96]
